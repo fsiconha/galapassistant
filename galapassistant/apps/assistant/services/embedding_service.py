28c00f294a1c7e967aa0de19726d8257f3b45766
Felipe Siconha <felipesiconha@gmail.com>
2025-03-26 17:13:23 -0300

    Adjust embedding service methods

--- galapassistant/apps/assistant/services/embedding_service.py
+++ galapassistant/apps/assistant/services/embedding_service.py
@@ -1,6 +1,7 @@
 import os
 from typing import List, Optional
 
+# from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
 from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain.docstore.document import Document as LangchainDocument
@@ -14,23 +15,23 @@ CHUNK_SIKE = 512
 MARKDOWN_SEPARATORS = [
     "\n\n",
     "\n",
-    ".",
-    " ",
+    # ".",
+    # " ",
 ]
 
 class EmbeddingService:
-    def load_knowledge_base(file_path: str) -> List[LangchainDocument]:
+    def load_knowledge_base(self, file_path: str) -> List[LangchainDocument]:
         with open(file_path, "r", encoding="utf-8") as f:
             content = f.read()
 
-        return [LangchainDocument(page_content=content)]   
+        return [LangchainDocument(page_content=content)]
 
-    def split_data(
+    def build_vector_database(
         self,
-        chunk_size: int = CHUNK_SIKE,
+        chunk_size: Optional[int] = CHUNK_SIKE,
         knowledge_base: Optional[List[LangchainDocument]] = None,
         tokenizer_name: Optional[str] = EMBEDDING_MODEL_NAME,
-    ) -> List[LangchainDocument]:
+    ):
         if knowledge_base is None:
             file_path = os.path.join(
                 os.path.dirname(__file__),
@@ -40,13 +41,15 @@ class EmbeddingService:
             )
             knowledge_base = self.load_knowledge_base(file_path)
 
-        text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-            AutoTokenizer.from_pretrained(tokenizer_name),
-            chunk_size=chunk_size,
-            chunk_overlap=int(chunk_size / 10),
-            add_start_index=True,
-            strip_whitespace=True,
-            separators=MARKDOWN_SEPARATORS,
+        text_splitter = (
+            RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+                AutoTokenizer.from_pretrained(tokenizer_name),
+                chunk_size=chunk_size,
+                chunk_overlap=int(chunk_size / 10),
+                add_start_index=True,
+                strip_whitespace=True,
+                separators=MARKDOWN_SEPARATORS,
+            )
         )
 
         doc_chunks = []
@@ -60,18 +63,17 @@ class EmbeddingService:
                 unique_texts[doc.page_content] = True
                 doc_chunks_unique.append(doc)
 
-        return doc_chunks_unique
-
-    def build_vector_database(self, doc_chunks):   ### PRECISA CACHEAR DE ALGUMA FORMA OU OTIMIZAR. Como usar o retriever sem buildar o vector store toda hora?
         embedding_model = HuggingFaceEmbeddings(
             model_name=EMBEDDING_MODEL_NAME,
-            multi_process=True,
-            model_kwargs={"device": "cpu"},
+            # multi_process=True,
+            # model_kwargs={"device": "cpu"},
             encode_kwargs={"normalize_embeddings": True},
         )
 
         knowledge_vector_database = FAISS.from_documents(
-            doc_chunks, embedding_model, distance_strategy=DistanceStrategy.COSINE
+            doc_chunks_unique,
+            embedding_model,
+            distance_strategy=DistanceStrategy.COSINE
         )
 
         return knowledge_vector_database
